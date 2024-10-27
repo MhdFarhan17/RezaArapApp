8e0fccb9aa449ef1edf26ea726d48ec93911ad54
MhdFarhan17 <mfsn2806.com>
2024-10-27 15:02:50 +0700

    Update Fitur Bot Discord

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'autorack.proxy.rlwy.net', // Sesuaikan dengan host dari Railway
-    user: process.env.MYSQLUSER, // pastikan variabel ini disetel di environment Railway
-    password: process.env.MYSQLPASSWORD, // pastikan variabel ini disetel di environment Railway
-    database: process.env.MYSQLDATABASE, // pastikan variabel ini disetel di environment Railway
-    port: 22700, // Gunakan port yang sesuai dari informasi Railway
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
+    queueLimit: 0
 });
 
 module.exports = pool;
