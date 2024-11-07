4f8b2de2cc15975ccabc1c8413cd5512e3ed0a3f
MhdFarhan17 <mfsn2806.com>
2024-11-07 12:03:37 +0700

    Update Bot Discord

--- commands/music.js
+++ commands/music.js
@@ -25,10 +25,8 @@ module.exports = {
         const content = message.content.toLowerCase();
 
         if (!isMusicLink(content) && !isValidMusicCommand(content)) {
-            message.delete().then(() => {
-                sendWarning(message, `${message.author}, di channel ini hanya diperbolehkan mengirim link YouTube, Spotify, atau menggunakan perintah musik yang valid.`);
-                logMessageDelete(client, message.guild.id, message.author.id, message.channel.id, content);
-            }).catch(console.error);
+            // Hanya kirim peringatan tanpa menghapus pesan
+            sendWarning(message, `${message.author}, di channel ini hanya diperbolehkan mengirim link YouTube, Spotify, atau menggunakan perintah musik yang valid.`);
         }
     }
 };
