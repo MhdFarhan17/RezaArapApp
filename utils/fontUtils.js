c4dae80f876110a8b1b67f466b53684e9c65f7da
MhdFarhan17 <mfsn2806.com>
2024-11-07 12:36:39 +0700

    Update Bot Discord

--- utils/fontUtils.js
+++ utils/fontUtils.js
@@ -1,12 +1,23 @@
 function convertToFancyFonts(text) {
-    const fancyFonts = [
-        text.split('').map(char => `𝐅${char}`).join(''), // Gaya 1
-        text.split('').map(char => `𝑭${char}`).join(''), // Gaya 2
-        text.split('').map(char => `𝓕${char}`).join(''), // Gaya 3
-        text.split('').map(char => `𝗙${char}`).join(''), // Gaya 4
-        text.split('').map(char => `𝔉${char}`).join(''), // Gaya 5
+    const styles = [
+        { name: 'Bold Serif', map: char => String.fromCodePoint(char.charCodeAt(0) + 0x1D400 - 0x41) },
+        { name: 'Italic Serif', map: char => String.fromCodePoint(char.charCodeAt(0) + 0x1D434 - 0x41) },
+        { name: 'Bold Script', map: char => String.fromCodePoint(char.charCodeAt(0) + 0x1D4D0 - 0x41) },
+        { name: 'Fraktur', map: char => String.fromCodePoint(char.charCodeAt(0) + 0x1D504 - 0x41) },
+        { name: 'Double-Struck', map: char => String.fromCodePoint(char.charCodeAt(0) + 0x1D538 - 0x41) }
     ];
-    return fancyFonts;
+
+    return styles.map(style => {
+        const formattedText = text.split('').map(char => {
+            if (/[A-Z]/.test(char)) {
+                return style.map(char);
+            } else if (/[a-z]/.test(char)) {
+                return String.fromCodePoint(char.charCodeAt(0) + (style.map('A').codePointAt(0) - 'A'.codePointAt(0)) + 0x20);
+            }
+            return char;
+        }).join('');
+        return formattedText;
+    });
 }
 
 module.exports = { convertToFancyFonts };
