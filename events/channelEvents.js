1b97bcc2be02067df8d51b48cd2dc7bb2d990cb8
MhdFarhan17 <mfsn2806.com>
2024-12-10 22:39:35 +0700

    Update Bot Discord YB

--- events/channelEvents.js
+++ events/channelEvents.js
@@ -15,9 +15,9 @@ module.exports.channelUpdate = {
         if (!newChannel.guild) return; // Pastikan event terjadi di dalam server (guild)
         const oldName = oldChannel.name;
         const newName = newChannel.name;
-        
+
         if (oldName !== newName) {
-            logChannelChange(client, newChannel.guild.id, 'Renamed', newChannel.id, `${oldName} ➔ ${newName}`);
+            logChannelChange(client, newChannel.guild.id, 'Renamed', newChannel.id, oldName, newName);
             console.log(`Channel '${oldName}' diubah menjadi '${newName}'.`);
         }
     }
@@ -27,7 +27,10 @@ module.exports.channelDelete = {
     name: 'channelDelete',
     async execute(channel, client) {
         if (!channel.guild) return; // Pastikan event terjadi di dalam server (guild)
-        logChannelChange(client, channel.guild.id, 'Deleted', channel.id, channel.name);
-        console.log(`Channel '${channel.name}' telah dihapus.`);
+
+        // Cek apakah nama channel tersedia, jika tidak, gunakan 'Unknown'
+        const channelName = channel.name || 'Unknown';
+        logChannelChange(client, channel.guild.id, 'Deleted', channel.id, channelName);
+        console.log(`Channel '${channelName}' telah dihapus.`);
     }
 };
